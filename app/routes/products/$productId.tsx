4917054dd99728a123c99ed3cf2123f7661fc91f
Kyle Manning <kyle@manning.jp>
2023-03-16 12:17:45 +0900

    initial commit

---
+++ app/routes/products/$productId.tsx
@@ -0,0 +1,26 @@
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({context}:LoaderArgs) => {
+    return json(
+        await context.kmj.get('kyle',{
+            type: "json",
+        })
+    )
+}
+
+export default function Product() {
+  const product = useLoaderData<typeof loader>();
+
+  if (!product) throw new Response(null, { status: 404 })
+
+  return (
+    <div>
+      <p>Product</p>
+      {product.name}
+      <p>Products</p>
+      {/* ... */}
+    </div>
+  );
+}
